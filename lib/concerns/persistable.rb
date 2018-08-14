8a3c244f50bc57275c1934bf0e2a7ffaf850be35
Sav Younts <savyounts@gmail.com>
2018-08-14 21:21:19 +0000

    automatically backed up by learn

--- lib/concerns/persistable.rb
+++ lib/concerns/persistable.rb
@@ -2,6 +2,15 @@ module Persistant
   module InstanceMethods 
     
   end 
+  
+  
   module ClassMethods
+    def save 
+      self.class.all << self
+    end
+  
+    def self. destroy_all 
+      self.all.clear
+    end
   end 
 end
